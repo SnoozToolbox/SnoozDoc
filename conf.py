e0913b47fa0a76a528841bd32e2f18730b9559fe
David Lévesque <david.levesque.cnmtl@ssss.gouv.qc.ca>
2023-06-20 15:35:23 -0400

    Trying to fix theme on readthedocs

--- conf.py
+++ conf.py
@@ -30,9 +30,9 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 #html_theme = 'sphinx_rtd_theme'
-import sphinx_pdj_theme
+#import sphinx_pdj_theme
 html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
